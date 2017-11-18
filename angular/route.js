6aece1f6bff70d61c0f98681f12842ab10885f57
laffingragons <akshaypatil,vit@gmail.com>
2017-11-18 22:34:04 +0530

    index-veiw ready
    index-veiw ready.
    working on individual match details.
    js is ready, working on UI

--- angular/route.js
+++ angular/route.js
@@ -8,6 +8,17 @@ myApp.config(['$routeProvider', function($routeProvider){
             // what is the alias of that controller.
         	controllerAs 	: 'epl'
         })
-      
+      .when('/match/:id1/:id2/:matchDate',{
+            // location of the template
+        	templateUrl		: 'views/matchDetails-view.html',
+        	// Which controller it should use 
+            controller 		: 'matchDetails',
+            // what is the alias of that controller.
+        	controllerAs 	: 'mD'
+        })
     
+}]);
+
+myApp.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('');
 }]);
