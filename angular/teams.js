9fc218d3ff1e2ca4ab268304660c56df6b367776
laffingragons <akshaypatil,vit@gmail.com>
2017-11-21 17:10:20 +0530

    trying loadash

--- angular/teams.js
+++ angular/teams.js
@@ -5,6 +5,8 @@ myApp.controller("teams",['$http','$location','$routeParams',function($http,$loc
     
     // var for data
     this.teamArray=[];
+    this.dup=[];
+    this.final=[];
     
     this.team15=function(data){
         
@@ -12,18 +14,17 @@ myApp.controller("teams",['$http','$location','$routeParams',function($http,$loc
         
         for(var x in main.rounds){
             for(var y in main.rounds[x].matches){
-                
-               function removeDuplicateNames() {
-    var realArray = [];
-    main.rounds[x].matches.team1.name.forEach(function(value,index) { 
-           (realArray.indexOf(main.rounds[x].matches[y].team1.name[index]) == -1) ? realArray.push(main.rounds[x].matches[y].team1.name[index]): ' ';                //conditional statement
-    });
+               
+            main.teamArray= _.map(main.rounds[x].matches[y].team1.name);
+                main.dup= main.teamArray.join("");
+                main.final= _.uniqBy(main.dup)
+                console.log(main.final);
+            
+           /*     function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
 }
-                
-                removeDuplicateNames();
-                console.log(main.teamArray);
-                
-                
+                main.teamArray= .filter( onlyUnique );
+                console.log(main.teamArray)  */
             }
         }
     }
